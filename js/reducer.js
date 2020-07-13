3de883465926fe2e3a5ea94aab0c073e550ddbad
pharia-le <pharia.le@gmail.com>
2020-07-13 13:15:08 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,9 +1,17 @@
-let state = {count: 0};
+let state
 
-function changeState(state, action){
+function changeState(state = {count: 0}, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
+      default:
+        return state;
+    }
+  }
+  function changeState(state = { count: 0 }, action) {
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 }
       default:
         return state;
     }
